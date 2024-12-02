7836d82a6767349a1fe2134cddb047c1e79ef712
nemeusa <nemeusa@gmail.com>
2024-12-02 07:43:23 -0300

    light clean

--- Assets/Scripts/Generics/LightActive.cs
+++ Assets/Scripts/Generics/LightActive.cs
@@ -4,15 +4,4 @@ using UnityEngine;
 
 public class LightActive : Lighting<Light>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
